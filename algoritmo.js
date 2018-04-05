78d5834dffdb30a16a4a824a9756ab5f5df944b9
GabrieleCamara <camaragabriele@gmail.com>
2018-04-05 17:23:36 -0300

    Arquivos da aula colaborativa

--- algoritmo.js
+++ algoritmo.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-	
+
 
   //1 - Mapa centrado em Curitiba
 	var map = L.map("meumapa", {
@@ -10,10 +10,53 @@ window.onload = function() {
 		minZoom: 4.5,
 		maxZoom: 18
 	});
-  
+
   //2 - Camadas base
-	
-	var osmColorido = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
-  
-  
+
+	var osmColorido = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+
+	//3 - Adicionando a base
+	var basemap4 = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+}).addTo(map);
+
+	//4 - Adicionando o WMS
+	var wms4 = L.tileLayer.wms('http://www.idea.ufpr.br/geoserver/geonode/wms', {
+	layers:'geonode:ocupacao_irregular',
+	transparent: 'true',
+	format: 'image/png'
+}).addTo(map);
+
+	//5 - Criando a classe dos símbolos personalizados
+	var Simbolo = L.Icon.extend ({
+		options: {
+			iconSize: [20, 20],
+			iconAnchor: [10, 10],
+			popupAnchor: [0, -10]
+	}
+	});
+
+	//5 - Armazenando as imagens svg em um vetor
+	var simbolo = [];
+	for(var i=0; i<=174; i++) {
+		simbolo[i] = new Simbolo({iconUrl: "dados/simbolos/" + i + ".svg"});
+	}
+
+	//5 - Adicionando o geoJSON
+	L.geoJSON(geoj4, {
+		pointToLayer: function(feicao, posicao) {
+			return L.marker(posicao, {icon: simbolo[85]});
+		}
+	}).addTo(map);
+
+	//6 - Popup com as coordenadas no click do usuario
+
+	// map.on("click", function(evento) {
+	// 	L.popup()
+  //   .getLatLng(latlng)
+  //   .setContent('<p>Hello world!<br />This is a nice popup.</p>')
+  //   .openOn(map);
+	// });
 }
