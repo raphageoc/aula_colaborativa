23ab38ca283fe2d736c50247e81be1b43feebc0b
jaquelinepisetta <jaquelinepisetta@gmail.com>
2018-04-05 17:09:52 -0300

    adicionei label da camada de bairros

--- algoritmo.js
+++ algoritmo.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-	
+
 
   //1 - Mapa centrado em Curitiba
 	var map = L.map("meumapa", {
@@ -10,10 +10,54 @@ window.onload = function() {
 		minZoom: 4.5,
 		maxZoom: 18
 	});
-  
+
   //2 - Camadas base
-	
-	var osmColorido = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
-  
-  
+
+	//Mapbox
+var basemap7 = L.tileLayer(
+'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
+{
+id:"mapbox.high-contrast",
+accessToken: "pk.eyJ1IjoiamFxdWVsaW5lcGlzZXR0YSIsImEiOiJjamYycmIxa3AwMXUzMnJvN2pjbTJpOWp5In0.4h6LRhENxZViHfwoaFVZjQ"
+}
+).addTo(map);
+
+
+//Adicionar camada WMS ao mapa
+L.tileLayer.wms("http://www.idea.ufpr.br/geoserver/geonode/wms"
+, {
+layers: "geonode:cmei",
+transparent: "true",
+format: "image/png"
+}).addTo(map);
+
+//adicionando o shape geojason ciclovia
+L.geoJSON(geojson7, {
+  style: function(feicao){
+        return{
+      weight: 3,
+      color: "#FF0000",
+      fillOpacity: 0
+    }
+  },
+  onEachFeature: function (feicao, camada){
+    camada.bindTooltip(feicao.properties.TIPO)
+  }
+}).addTo(map);
+
+var estiloLinhaPonto = {
+	color: "#00008B",
+	fillColor: "#FA8072",
+	weight: 2,
+	opacity: 50,
+	fillOpacity: 0
+};
+//adicionando o shape
+ L.geoJSON(divisadebairros, {
+ style: estiloLinhaPonto,
+ onEachFeature: function (feicao, camada){
+ 	camada.bindTooltip(feicao.properties.NOME)
+  }
+}).addTo(map);
+
 }
