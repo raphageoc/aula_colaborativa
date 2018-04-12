abb29da34dc90e50436c18c3422ceac499c78416
alexbk22 <alexbkraemer@gmail.com>
2018-04-12 14:54:14 -0300

    Com a barra de ferramenta funcionando

--- algoritmo.js
+++ algoritmo.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-	
+
 
   //1 - Mapa centrado em Curitiba
 	var map = L.map("meumapa", {
@@ -10,10 +10,48 @@ window.onload = function() {
 		minZoom: 4.5,
 		maxZoom: 18
 	});
-  
+
   //2 - Camadas base
-	
-	var osmColorido = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
-  
-  
+
+	var osmColorido = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+
+	var basemap1 = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/Specialty/DeLorme_World_Base_Map/MapServer/tile/{z}/{y}/{x}",{
+	               attribution: "Tiles &copy; Esri &mdash; Copyright: &copy;2012 DeLorme",
+	               minZoom: 1,
+	               maxZoom: 20
+							 }).addTo(map);
+
+  //Adicionar camada WMS ao mapa
+	var wms1 = L.tileLayer.wms("http://www.idea.ufpr.br/geoserver/geonode/wms", {
+             layers: "geonode:altimetria_ippuc_curitiba_wgs84",
+						 transparent: "true",
+             format: "image/png"
+             }).addTo(map);
+
+  //Adicionar GeoJson
+	var estilogeojson1 = {
+      color: "#41ae76",
+			fillColor: "#ccebc5",
+
+			weight: 1,
+			opacity: 1,
+			fillOpacity: 0.4
+	    };
+
+  //Aplicar estilos ao criar as camadas GeoJSON
+	L.geoJSON(geojson1, {
+      style: estilogeojson1
+	    }).addTo(map);
+
+  //Adicionar barra de ferramentas
+	var barraferramentas = L.control.navbar({
+                position: "topleft"
+	              }).addTo(map);
+
+	/*//Adicionar segunda barra de barraferramentas
+	new L.Toolbar2.Control({
+		       positions: "topleft"
+	         //actions: [MyAction1, MyAction2]
+				 }).addTo(map);*/
+
 }
