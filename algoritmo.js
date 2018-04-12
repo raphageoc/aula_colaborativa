50ea15cf185c575cca656152a28cdfe1b5bddee5
lucasbiscaro <lucashebiscaro@hotmail.com>
2018-04-12 15:01:56 -0300

    Commit 12/04/2018

--- algoritmo.js
+++ algoritmo.js
@@ -7,7 +7,7 @@ window.onload = function() {
 		zoom: 15,
 		zoomSnap: 0.5,
 		zoomDelta: 0.5,
-		minZoom: 4.5,
+		minZoom: 0,
 		maxZoom: 18
 	});
 
@@ -28,10 +28,15 @@ window.onload = function() {
 	}).addTo(map);
 
 
-	//Criar classe dos simbolos
+	//First, specify your Mapbox API access token
+	L.MakiMarkers.accessToken = "pk.eyJ1IjoibHVjYXNiaXNjYXJvIiwiYSI6ImNqZjJyY2NiNDAwcTgzM2x0czlpbnA0cTkifQ.MvRGPuxzJSBW_wH_MbeY0g";
+
+	var icon = L.MakiMarkers.icon({icon: "park", color: "#40b64a", size: "m"});
+
+	// Criar classe dos simbolos
 	var Simbolo = L.Icon.extend({
 	options: {
-	iconSize: [40, 40],
+	iconSize: [30, 30],
 	iconAnchor: [25, 25],
 	popupAnchor: [0, -30]
 	}
@@ -40,26 +45,61 @@ window.onload = function() {
 	//Adicionando símbolos
 	var simbolo = [];
 
-	for(var i=0; i<=174; i++){
+	for(var i=0; i<=175; i++){
 		simbolo[i] = new Simbolo({iconUrl: "dados/simbolos/"+ i +".svg"});
 	}
 
 	//Adicionando o arquivo GeoJSON
 	var geoj9 = L.geoJSON(geoj, {
 		pointToLayer: function(feicao,posicao) {
-			return L.marker(posicao, {icon:simbolo[82]});
+			return L.marker(posicao, {icon: icon});
 		}
 	}).addTo(map);
 
 	//Controle de Camadas
 	var basecarto = {
 		"OpenStreetMap": osmColorido,
-		"Black and White": basemap9
+		"OSM Black and White": basemap9,
+		"Esri De Lorme": basemap1,
+		"Mapbox Comic": basemap2,
+		"Mapbox Dark": basemap6,
+		"Mapbox Street": basemap8,
+		"Mapbox Outdoors": basemap5,
+		"Mapbox High Contrast": basemap7,
+		"Esri World Imagery": basemap10,
+		"OSM Hot": basemap11,
+		"OSM De": basemap12,
+		"Carto DB Positron": basemap4,
+		"Mapbox Street Satellite": basemap14,
+	  "Open Topo Map": basemap15
 	};
 
 	var feicoes = {
-		"Ciclovias": wms9,
-		"academias": geoj9
+		"Ciclovias WMS": wms9,
+	  "Altimetria WMS": wms1,
+		"Hospitais WMS": wms2,
+		"Lotes WMS": wms6,
+		"Academias ao ar livre WMS": wms8,
+		"Terminais de Transporte WMS": wms5,
+		"Creche/Jardinete WMS": wms7,
+		"Hidrografia WMS": wms10,
+		"Escola Municipal WMS": wms11,
+		"Ferrovias WMS": wms12,
+		"Ocupações Irregulares WMS": wms4,
+		"CAPs (Centro de Atenção Psicosocial) WMS": wms15,
+		"academias JSON": geoj9
+		"Divisas/Regionais JSON": geoj6,
+		"Cemitérios JSON": geoj2,
+		"Altimetria JSON": geojson1,
+		"Terminais de Transpote JSON": geoj8,
+		"Ruas da Cidadania JSON": geoj5,
+		"Ciclovias JSON": geo7,
+		"Hidrografia Polígono JSON": geojson10,
+		"Praças e Jardinetes JSON": geoj11,
+		"Eixos de Rua JSON": geoj12,
+		"Escolas Municipais JSON": geoj4,
+		"Divisas de Bairro JSON": geo14,
+		"Unidades de Saúde JSON": geo15
 	};
 
 	//Adicionar objetos ao controle de camadas
